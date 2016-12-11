03ce5bde52fd92f2b5c5d53de5e3f506c9bd3292
JennyB008 <jennifer.brenner@web.de>
2016-12-11 11:09:01 +0100

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,33 +1,28 @@
-//
-// Created by Patrick on 22.11.2016.
-//
-
-#include <iostream>
 #include "Matrix.h"
+#include <iostream>
 
 static int counter = 0;
 
 Matrix::Matrix(void) : m_Zeilen(2), m_Spalten(1) {
-    std::cout << "Standardkonstruktor wird aufgerufen." << std::endl;
-    for (int i = 0; i < m_Zeilen * m_Spalten; i++)
-        m_Element[i] = 0;
-    counter += 1;
+	std::cout << "Standardkonstruktor wird aufgerufen." << std::endl;
+	m_Element[0] = m_Zeilen;
+	m_Element[1] = m_Spalten;
+	counter += 1;
 }
 
 Matrix::~Matrix() {
-    for (int i = 0; i < m_Zeilen * m_Spalten; i++) {
-        std::cout << m_Element[i] << " ";
-    }
-    std::cout << "Matrix wurde zerstört" << std::endl;
-    counter -= 1;
+	std::cout << "Matrix ";
+	ausgabe();
+	std::cout << "wird zerstoert." << std::endl;
+	counter -= 1;
 }
 
 void Matrix::ausgabe() {
-    for (int i = 0; i < m_Zeilen * m_Spalten; i++)
-        std::cout << m_Element[i] << " ";
-    std::cout << std::endl;
+	for (int i = 0; i < m_Zeilen * m_Spalten; i++)
+		std::cout << m_Element[i] << " ";
+	std::cout << std::endl;
 }
 
 int Matrix::getCounter() {
-    return counter;
+	return counter;
 }
